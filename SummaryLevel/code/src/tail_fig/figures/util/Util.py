007662a4231faa976148cc04ee156628021a72fb
Tade <tade.souaiaia@gmail.com>
2026-04-12 16:17:20 -0400

    text outlines off

--- SummaryLevel/code/src/tail_fig/figures/util/Util.py
+++ SummaryLevel/code/src/tail_fig/figures/util/Util.py
@@ -17,12 +17,18 @@ plt.rcParams['xtick.major.size'] = 2
 plt.rcParams['ytick.major.size'] = 2
 plt.rcParams['xtick.labelsize'] = 5
 plt.rcParams['ytick.labelsize'] = 5
+plt.rcParams['axes.labelpad'] = 1
 matplotlib.rcParams['xtick.direction'] = 'inout'                                                                                                                                    
 matplotlib.rcParams['ytick.direction'] = 'inout'                                                                                                                                   
 matplotlib.rcParams['xtick.major.pad'] = 0.5
 matplotlib.rcParams['ytick.major.pad'] = 0.5
 matplotlib.rcParams['axes.linewidth'] = 0.35
-plt.rcParams['axes.labelpad'] = 1
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
+matplotlib.rcParams['tex.usetex'] = False
+
+
+
 
 
 
